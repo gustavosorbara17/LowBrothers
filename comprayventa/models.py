03d0d376c73e92edd13559eab511a9e38816bc71
GustavoSorbara <gustydebera2013@gmail.com>
2017-06-16 08:58:50 -0300

    Actualizadas URLs

--- comprayventa/models.py
+++ comprayventa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class auto(models.Model):
+	marca = models.CharField(max_length=200)
+	modelo = models.CharField(max_length=200)
+	patente = models.CharField(max_length=200)
+	tipo = models.CharField(max_length=200)
+	color = models.CharField(max_length=200)
+	precio = models.DecimalField(max_digits=20, decimal_places=10)
