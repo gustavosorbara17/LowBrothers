3073ae6e106856f24eab48c2242bdaf49a49cfcd
gustavosorbara17 <gustydebera2013@gmail.com>
2017-07-06 09:17:33 -0300

    mod html

--- comprayventa/views.py
+++ comprayventa/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Auto
 
 # Create your views here.
 
 def post_list(request):
-        return render(request, 'html.html', {})
-        
+    autosvar = Auto.objects.all()
+    return render(request, 'comprayventa/html.html', {'listaAutos': autosvar})
+
+
         
